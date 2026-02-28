62057e73190467cf3d9059939d98796d8dd1b6bd
root <root@localhost.localdomain>
2026-02-28 13:44:47 +0000

    Remove GitHub Pages config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/',
   plugins: [react()],
   build: {
     target: 'esnext'
